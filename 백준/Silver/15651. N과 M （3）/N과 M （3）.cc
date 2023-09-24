cfa8b84afa4672a88c13da2fb7ff502d8c3bf99a
KKPASII <90451738+KKPASII@users.noreply.github.com>
2023-09-24 22:51:59 +0900

    [Silver III] Title: N과 M (3), Time: 584 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[7];
+bool isused[7];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		arr[k] = i;
+		isused[i] = 1;
+		func(k + 1);
+		isused[i] = 0;
+	}
+}
+
+int main(void) {
+
+	cin >> n >> m;
+	func(0);
+
+	return 0;
+}
