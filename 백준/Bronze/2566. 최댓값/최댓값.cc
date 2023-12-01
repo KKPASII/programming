318101f2688f83ac46d749d61e30072d4aecd715
KKPASII <90451738+KKPASII@users.noreply.github.com>
2023-12-01 20:44:42 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define MAX 9
+
+int main(void) {
+	int n= 0, m=0,temp, max = -1;
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) {
+			cin >>temp;
+			if (max <= temp) {
+				max = temp; n = i; m = j;
+			}
+		}
+	}
+
+	cout << max << '\n';
+	cout << n +1 << ' ' << m + 1;
+	return 0;
+}
