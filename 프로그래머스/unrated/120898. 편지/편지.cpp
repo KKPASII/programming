9e84adf8a7cb7c0e034ef479f0b629e37c0a3c9b
KKPASII <90451738+KKPASII@users.noreply.github.com>
2023-12-17 21:19:36 +0900

    [level unrated] Title: 편지, Time: 0.01 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    int len = message.length();
+    answer = 2 * len;
+    return answer;
+}
