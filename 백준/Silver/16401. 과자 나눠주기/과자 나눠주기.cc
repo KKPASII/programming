4d6e95f86abe1ff6d7cc7f828b3c5148e240234f
KKPASII <90451738+KKPASII@users.noreply.github.com>
2024-01-24 19:45:50 +0900

    [Silver II] Title: 과자 나눠주기, Time: 208 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+	int n, m, temp;
+	cin >> m >> n;
+	vector<int> snacks;
+	for (int c = 0; c < n; c++) {
+		cin >> temp; snacks.push_back(temp);
+	}
+	int start = 1;
+	int end = *max_element(snacks.begin(), snacks.end());
+	int maxLen = 0, sum = 0;
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		int sum = 0;
+		for (auto& snk : snacks)
+			sum += snk / mid;
+		if (sum >= m) {
+			maxLen = mid;
+			start = mid + 1;
+		}
+		else
+			end = mid - 1;
+	}
+	cout << maxLen;
+	return 0;
+}
