7ffdef582e009ec8c946af7fdc77a134f249bcdf
KKPASII <90451738+KKPASII@users.noreply.github.com>
2024-01-25 22:55:35 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -1,44 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-void dfs(int number, int jarisu);
-bool isPrime(int number);
-
-static int N;
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N;
-	dfs(2, 1);	// param: 소수, 해당 소수의 자릿수
-	dfs(3, 1);
-	dfs(5, 1);
-	dfs(7, 1);
-
-	return 0;
+int n;
+bool isPrime(int n) {
+    for (int i = 2; i <= sqrt(n); i++)
+        if (n % i == 0) return false;
+    return true;
 }
-
-void dfs(int number, int jarisu) {
-	if (jarisu == N) {
-		if (isPrime(number)) {	// 소수이면
-			cout << number << '\n';
-		}
-		return;
-	}
-
-	for (int i = 1; i < 10; i++) {
-		if (i % 2 == 0) continue;	// 짝수이면 pass
-		if (isPrime(number * 10 + i)) {		// 소수이면
-			dfs(number * 10 + i, jarisu + 1);		// 자리수 늘려서 다시 탐색 dfs(소수로 판명된 수, 늘린 자릿 수)
-		}
-	}
+void dfs(int num, int jarisu) {
+    if (jarisu == n) {
+        if (isPrime(num)) cout << num << '\n';
+        return;
+    }
+    for (int i = 1; i < 10; i++) {
+        if (i % 2 == 0) continue;
+        int number = num * 10 + i;
+        if (isPrime(number))
+            dfs(number, jarisu + 1);
+    }
 }
-
-bool isPrime(int number) {
-	for (int i = 2; i <= number / 2; i++) {
-		if (number % i == 0) return false;
-	}
-	return true;
+int main() {
+    cin >> n;dfs(2, 1);dfs(3, 1);dfs(5, 1);dfs(7, 1);
+    return 0;
 }
