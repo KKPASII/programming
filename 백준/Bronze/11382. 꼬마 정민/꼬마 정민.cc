895aff3608df6f99b54dc10aa2536f5be5cc7163
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-09-30 23:41:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long answer = 0;
+    long long a, b, c;
+    cin >> a >> b >> c;
+    answer = a + b + c; 
+    cout << answer;
+    return 0;
+}
