780b507d3d2fbe432f272d3eedd91eaeedb8d90c
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-10-15 23:06:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.09 ms, Memory: 5.16 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -7,17 +7,14 @@ vector<int> solution(string s) {
     vector<int> answer;
     unordered_map<char, int> map;
     int len = s.size();
-    char cur_char;
     for (int i = 0; i < len; i++) {
-        cur_char = s[i];
-        if (map.find(cur_char) != map.end()) {
-            answer.push_back(i - map[cur_char]);
-            map[cur_char] = i;
+        if (map.find(s[i]) != map.end()) {
+            answer.push_back(i - map[s[i]]);
         }
         else {
-            map[cur_char] = i;
             answer.push_back(-1);
         }
+        map[s[i]] = i;
     }
     return answer;
 }
