f6df379d6bc359fac7a3b52d0575ac6863d853c7
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-10-17 00:58:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,25 +1,16 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <regex>
 using namespace std;
 
 int solution(string s) {
-    map<string, char> m = {{"zero",'0'}, {"one",'1'}, {"two",'2'}, {"three",'3'}, {"four",'4'}, 
-                           {"five",'5'}, {"six",'6'}, {"seven",'7'}, {"eight",'8'}, {"nine",'9'}};
-    string answer = "";
-    int len = s.size();
-    string str = "";
+    vector<string> words = {"zero", "one", "two", "three", "four", "five", "six", "seven","eight","nine"};
+    string answer = s;
+    regex r;
+    int len = words.size();
     for (int i = 0; i < len; i++) {
-        if (s[i] < 58) {
-            answer += s[i];
-        }
-        else {
-            str += s[i];
-            if (m.find(str) != m.end()) {
-                answer += m[str];
-                str = "";
-            }
-        }
+        r = words[i];
+        answer = regex_replace(answer, r, to_string(i));
     }
     return stoi(answer);
 }
