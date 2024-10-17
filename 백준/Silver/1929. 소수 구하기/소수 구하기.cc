aa48744e1831675fa3897eafb5e2ca1df7ce5217
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-10-18 04:21:22 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,8 +2,11 @@
 #include <cmath>
 using namespace std;
 bool isPrime(int num) {
-    if (num < 2) return false;
-	for (int i = 2; i <= sqrt(num); i++) {
+	if (num < 2) return false;
+	if (num == 2) return true;
+	if (num % 2 == 0) return false;
+	int limit = sqrt(num);
+	for (int i = 3; i <= limit; i++) {
 		if (num % i == 0) return false;
 	}
 	return true;
