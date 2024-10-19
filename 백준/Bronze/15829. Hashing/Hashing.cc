fe7e245b6070fee8daf97b49e87124cea598a22e
Hamplz <90451738+KKPASII@users.noreply.github.com>
2024-10-20 00:46:33 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,17 +1,17 @@
 #include <iostream>
-#define MOD 1234567891
+#include <string>
 using namespace std;
 int main() {
-	cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-	char ch; int n, r = 1;
-	cin >> n;
-    long long result = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> ch;
-		int num = ch - 'a' + 1;
-		result = (result + (num * r)) % MOD;
-		r = (r * 31) % MOD;
-	}
-	cout << result;
-	return 0;
+    int L;
+    string str;
+    cin >> L >> str;
+    long long hash = 0;
+    long long r = 1;
+    const int M = 1234567891;
+    for (int i = 0; i < L; i++) {
+        hash = (hash + (str[i] - 'a' + 1) * r) % M;
+        r = (r * 31) % M;
+    }
+    cout << hash << endl;
+    return 0;
 }
