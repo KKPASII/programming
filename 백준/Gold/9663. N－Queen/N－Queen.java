7afb93a27bed49493e0c51dc2c1ce7f8bc4fe7b7
Hamplz <90451738+KKPASII@users.noreply.github.com>
2025-01-05 18:26:07 +0900

    [Gold IV] Title: N-Queen, Time: 2492 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -3,32 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[] arr;
     static int N;
-    static int answer = 0;
+    static int[] v1;
+    static int[] v2;
+    static int[] v3;
+    static int answer;
 
-    public static boolean check(int curRow) {
-        for (int i = 0; i < curRow; i++) {
-            if (arr[curRow] == arr[i]) {
-                return false;
-            }
-            else if(Math.abs(curRow-i) == Math.abs(arr[curRow]-arr[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void backtrack(int depth) {
-        if (depth == N) {
+    public static void backtrack(int x) {
+        if (x == N) {
             answer++;
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            arr[depth] = i;
-            if (check(depth)) {
-                backtrack(depth + 1);
+        for (int y = 0; y < N; y++) {
+            if (v1[y] == 0 && v2[x+y] == 0 && v3[x-y+N] == 0) {
+                v1[y] = 1;
+                v2[x+y] = 1;
+                v3[x-y+N] = 1;
+
+                backtrack(x+1);
+
+                v1[y] = 0;
+                v2[x+y] = 0;
+                v3[x-y+N] = 0;
             }
         }
     }
@@ -37,7 +34,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         N = Integer.parseInt(str);
-        arr = new int[N];
+        v1 = new int[N];
+        v2 = new int[2*N];
+        v3 = new int[2*N];
 
         backtrack(0);
         System.out.println(answer);
